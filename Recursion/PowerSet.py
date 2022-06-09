1aeb0df01d94a519bf1f2fc78d51e97bf8ecddac
rohit-shalgar <learn.rohits@.github.com>
2022-06-08 20:08:28 -0500

    Power Set optimized.

--- Recursion/PowerSet.py
+++ Recursion/PowerSet.py
@@ -10,3 +10,20 @@ def get_power_set(input_array: list):
             power_sets.append(current_power_set + [number])
 
     return power_sets
+
+
+def get_power_sets_optimized_helper(input_array, indx:int):
+    if indx == len(input_array):
+        return [[]]
+    else:
+        element = input_array[indx]
+        power_sets = get_power_sets_optimized_helper(input_array, indx+1)
+        power_sets_size = len(power_sets)
+        for i in range(power_sets_size):
+            power_set = power_sets[i]
+            power_sets.append(power_set + [element])
+        return power_sets
+
+
+def get_power_sets_optimized(input_array: list):
+    return get_power_sets_optimized_helper(input_array, 0)
--- Recursion/Test/PowerSetsTest.py
+++ Recursion/Test/PowerSetsTest.py
@@ -17,3 +17,17 @@ class PowerSetTest(unittest.TestCase):
         self.assertTrue([1, 2, 3] in power_sets)
         self.assertTrue([1, 3] in power_sets)
         self.assertTrue([2, 3] in power_sets)
+
+    def test_case_2_optimized(self):
+        power_sets = PowerSet.get_power_sets_optimized([1, 2, 3])
+        print(power_sets)
+        self.assertEqual(8, len(power_sets))
+        self.assertTrue([] in power_sets)
+        self.assertTrue([1] in power_sets)
+        self.assertTrue([2] in power_sets)
+        self.assertTrue([3] in power_sets)
+        self.assertTrue([2, 1] in power_sets)
+        self.assertTrue([3, 1] in power_sets)
+        self.assertTrue([3, 2, 1] in power_sets)
+        self.assertTrue([3, 1] in power_sets)
+        self.assertTrue([3, 2] in power_sets)
